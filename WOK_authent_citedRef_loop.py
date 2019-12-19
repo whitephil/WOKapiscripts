5b98f9897081a75524a6ff734ea3b6a7d529af1d
Phil White <philip.white@colorado.edu>
2019-12-19 13:44:37 -0700

    fixed type base10 to base64

--- WOK_authent_citedRef_loop.py
+++ WOK_authent_citedRef_loop.py
@@ -5,7 +5,7 @@ import csv
 a_url = 'http://search.webofknowledge.com/esti/wokmws/ws/WOKMWSAuthenticate?wsdl'
 a_client = Client(a_url)
 a_client.set_options(headers={'Authorization':'Basic '})
-                                                    #^base10 encoded username and pw goes here 
+                                                    #^base64 encoded username and pw goes here 
 ID = a_client.service.authenticate()
 SID = 'SID='
 SID += ID
